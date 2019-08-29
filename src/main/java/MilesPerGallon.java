6978f9a9339b50f44df628bd9de0ccfa8dd5b59b
0787461 <0787461@KELCE-LABS29-P.pittstate.edu>
2019-08-29 12:06:08 -0500

    3rd commit

---
+++ src/main/java/MilesPerGallon.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0787461
+ */
+public class MilesPerGallon {
+    public static void main(String[] args)
+    {
+        float milesDriven = 250.5F;
+        float gallonsUsed = 10;
+        float milesPerGallon = milesDriven / gallonsUsed;
+        
+        System.out.println("Miles per gallon:\t" + milesPerGallon);
+    }
+}
